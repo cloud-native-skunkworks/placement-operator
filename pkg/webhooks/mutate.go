d7de4cc3fdf511eb76e61d212662df8268719e84
Alex Jones <alexsimonjones@gmail.com>
2023-02-09 12:06:14 +0000

    init
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>

---
+++ pkg/webhooks/mutate.go
@@ -0,0 +1,49 @@
+package webhooks
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,failurePolicy=Ignore,groups="",resources=pods,verbs=create;update,versions=v1,name=mutate.cnskunkworks.io,admissionReviewVersions=v1,sideEffects=NoneOnDryRun
+//+kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=*,verbs=*;
+
+type PodMutator struct {
+	decoder *admission.Decoder
+	ready   bool
+	Log     logr.Logger
+}
+
+func (m *PodMutator) IsReady(_ *http.Request) error {
+	if m.ready {
+		return nil
+	}
+	return errors.New("pod mutator is not ready")
+}
+func (m *PodMutator) Handle(ctx context.Context, req admission.Request) admission.Response {
+	defer func() {
+		if err := recover(); err != nil {
+			admission.Errored(http.StatusInternalServerError, fmt.Errorf("%v", err))
+		}
+	}()
+	defer func() {
+		m.ready = true
+	}()
+
+	pod := &corev1.Pod{}
+	err := m.decoder.Decode(req, pod)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	return admission.Allowed("")
+}
